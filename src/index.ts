ed7047da716f2f6e3142b3ffa417a1480ac8f482
varunraj <varunraj82@gmail.com>
2020-06-09 12:19:39 -0400

    implemented charcollection class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection);
+
+const charaCollection = new CharactersCollection("Varun");
+const sortChar = new Sorter(charaCollection);
+sortChar.sort();
+console.log(charaCollection);
