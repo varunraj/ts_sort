46957d6beb458eb3f5dd31b9abf5554ad6baaac5
varunraj <varunraj82@gmail.com>
2020-06-09 10:32:28 -0400

    implemented Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi there!!!");
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection);
