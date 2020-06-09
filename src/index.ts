3c3ceffd947aa50d3d0c512bc66dd67726bc35b3
varunraj <varunraj82@gmail.com>
2020-06-09 13:38:49 -0400

    implemented linkedList collection class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charaCollection = new CharactersCollection("Varun");
 const sortChar = new Sorter(charaCollection);
 sortChar.sort();
 console.log(charaCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const llsorter = new Sorter(linkedList);
+llsorter.sort();
+console.log(linkedList);
